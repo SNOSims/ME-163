138c04566ff1efe70e21a299aeb3caa8775b677b
Level <LevelPulse@gmail.com>
2017-05-25 22:11:31 +0100

    Added takeoff mission - free-flight is broken atm. An old iteraiton.

--- Missions/QuickStart/quickstart.lua
+++ Missions/QuickStart/quickstart.lua
@@ -3,6 +3,10 @@ planes = {
     name = _('Free Flight'),
     file = 'me_163_free_flight.miz',
 	},
+	{ 
+    name = _('Take Off'),
+    file = 'me_163_take_off.miz',
+	},
 }
 
 --Zilch, please add mission examples here.
