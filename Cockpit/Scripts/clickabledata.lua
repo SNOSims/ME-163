93094b7a2de9a81f7ba1a5233c0d6e1b3266c4f8
Alex Wiese <LevelPulse@gmail.com>
2017-11-12 16:00:33 +0000

    arg 728

--- Cockpit/Scripts/clickabledata.lua
+++ Cockpit/Scripts/clickabledata.lua
@@ -15,4 +15,287 @@ cursor_mode =
 clickable_mode_initial_status  = cursor_mode.CUMODE_CLICKABLE
 use_pointer_name			   = true
 
-elements["oxygen_valve"] = default_axis("Oxygen Valve", devices.AVIONICS, device_commands.AltPressureKnob, 728, 0, 0.05, false, true)
+function default_button(hint_,device_,command_,arg_,arg_val_,arg_lim_)
+
+	local   arg_val_ = arg_val_ or 1
+	local   arg_lim_ = arg_lim_ or {0,1}
+
+	return  {	
+				class 				= {class_type.BTN},
+				hint  				= hint_,
+				device 				= device_,
+				action 				= {command_},
+				stop_action 		= {command_},
+				arg 				= {arg_},
+				arg_value			= {arg_val_}, 
+				arg_lim 			= {arg_lim_},
+				use_release_message = {true}
+			}
+end
+
+function default_1_position_tumb(hint_, device_, command_, arg_, arg_val_, arg_lim_)
+	local   arg_val_ = arg_val_ or 1
+	local   arg_lim_ = arg_lim_ or {0,1}
+	return  {	
+				class 		= {class_type.TUMB},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command_},
+				arg 	  	= {arg_},
+				arg_value 	= {arg_val_}, 
+				arg_lim   	= {arg_lim_},
+				updatable 	= true, 
+				use_OBB 	= true
+			}
+end
+
+function default_2_position_tumb(hint_, device_, command_, arg_)
+	return  {	
+				class 		= {class_type.TUMB,class_type.TUMB},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command_,command_},
+				arg 	  	= {arg_,arg_},
+				arg_value 	= {1,-1}, 
+				arg_lim   	= {{0,1},{0,1}},
+				updatable 	= true, 
+				use_OBB 	= true
+			}
+end
+
+function default_3_position_tumb(hint_,device_,command_,arg_,cycled_,inversed_)
+	local cycled = true
+	
+	
+	local val =  1
+	if inversed_ then
+	      val = -1
+	end
+	if cycled_ ~= nil then
+	   cycled = cycled_
+	end
+	return  {	
+				class 		= {class_type.TUMB,class_type.TUMB},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command_,command_},
+				arg 	  	= {arg_,arg_},
+				arg_value 	= {val,-val}, 
+				arg_lim   	= {{-1,1},{-1,1}},
+				updatable 	= true, 
+				use_OBB 	= true,
+				cycle       = cycled
+			}
+end
+
+function default_axis(hint_,device_,command_,arg_, default_, gain_,updatable_,relative_)
+	
+	local default = default_ or 1
+	local gain = gain_ or 0.1
+	local updatable = updatable_ or false
+	local relative  = relative_ or false
+	
+	return  {	
+				class 		= {class_type.LEV},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command_},
+				arg 	  	= {arg_},
+				arg_value 	= {default}, 
+				arg_lim   	= {{0,1}},
+				updatable 	= updatable, 
+				use_OBB 	= true,
+				gain		= {gain},
+				relative    = {relative}, 				
+			}
+end
+
+function default_movable_axis(hint_,device_,command_,arg_, default_, gain_,updatable_,relative_)
+	
+	local default = default_ or 1
+	local gain = gain_ or 0.1
+	local updatable = updatable_ or false
+	local relative  = relative_ or false
+	
+	return  {	
+				class 		= {class_type.MOVABLE_LEV},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command_},
+				arg 	  	= {arg_},
+				arg_value 	= {default}, 
+				arg_lim   	= {{0,1}},
+				updatable 	= updatable, 
+				use_OBB 	= true,
+				gain		= {gain},
+				relative    = {relative}, 				
+			}
+end
+
+function default_axis_limited(hint_,device_,command_,arg_, default_, gain_,updatable_,relative_, arg_lim_)
+	
+	local relative = false
+	local default = default_ or 0
+	local updatable = updatable_ or false
+	if relative_ ~= nil then
+		relative = relative_
+	end
+
+	local gain = gain_ or 0.1
+	return  {	
+				class 		= {class_type.LEV},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command_},
+				arg 	  	= {arg_},
+				arg_value 	= {default}, 
+				arg_lim   	= {arg_lim_},
+				updatable 	= updatable, 
+				use_OBB 	= false,
+				gain		= {gain},
+				relative    = {relative},  
+			}
+end
+
+
+function multiposition_switch(hint_,device_,command_,arg_,count_,delta_,inversed_, min_)
+    local min_   = min_ or 0
+	local delta_ = delta_ or 0.5
+	
+	local inversed = 1
+	if	inversed_ then
+		inversed = -1
+	end
+	
+	return  {	
+				class 		= {class_type.TUMB,class_type.TUMB},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command_,command_},
+				arg 	  	= {arg_,arg_},
+				arg_value 	= {-delta_ * inversed,delta_ * inversed}, 
+				arg_lim   	= {{min_, min_ + delta_ * (count_ -1)},
+							   {min_, min_ + delta_ * (count_ -1)}},
+				updatable 	= true, 
+				use_OBB 	= true
+			}
+end
+
+function multiposition_switch_limited(hint_,device_,command_,arg_,count_,delta_,inversed_,min_)
+    local min_   = min_ or 0
+	local delta_ = delta_ or 0.5
+	
+	local inversed = 1
+	if	inversed_ then
+		inversed = -1
+	end
+	
+	return  {	
+				class 		= {class_type.TUMB,class_type.TUMB},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command_,command_},
+				arg 	  	= {arg_,arg_},
+				arg_value 	= {-delta_ * inversed,delta_ * inversed}, 
+				arg_lim   	= {{min_, min_ + delta_ * (count_ -1)},
+							   {min_, min_ + delta_ * (count_ -1)}},
+				updatable 	= true, 
+				use_OBB 	= true,
+				cycle     	= false, 
+			}
+end
+
+function default_button_axis(hint_, device_,command_1, command_2, arg_1, arg_2, limit_1, limit_2)
+	local limit_1_   = limit_1 or 1.0
+	local limit_2_   = limit_2 or 1.0
+return {
+			class		=	{class_type.BTN, class_type.LEV},
+			hint		=	hint_,
+			device		=	device_,
+			action		=	{command_1, command_2},
+			stop_action =   {command_1, 0},
+			arg			=	{arg_1, arg_2},
+			arg_value	= 	{1, 0.5},
+			arg_lim		= 	{{0, limit_1_}, {0,limit_2_}},
+			animated        = {false,true},
+			animation_speed = {0, 0.4},
+			gain = {0, 0.1},
+			relative	= 	{false, false},
+			updatable 	= 	true, 
+			use_OBB 	= 	true,
+			use_release_message = {true, false}
+	}
+end
+
+function default_animated_lever(hint_, device_, command_, arg_, animation_speed_,arg_lim_)
+local arg_lim__ = arg_lim_ or {0.0,1.0}
+return  {	
+	class  = {class_type.TUMB, class_type.TUMB},
+	hint   	= hint_, 
+	device 	= device_,
+	action 	= {command_, command_},
+	arg 		= {arg_, arg_},
+	arg_value 	= {1, 0},
+	arg_lim 	= {arg_lim__, arg_lim__},
+	updatable  = true, 
+	gain 		= {0.1, 0.1},
+	animated 	= {true, true},
+	animation_speed = {animation_speed_, 0},
+	cycle = true
+}
+end
+
+function default_button_tumb(hint_, device_, command1_, command2_, arg_)
+	return  {	
+				class 		= {class_type.BTN,class_type.TUMB},
+				hint  		= hint_,
+				device 		= device_,
+				action 		= {command1_,command2_},
+				stop_action = {command1_,0},
+				arg 	  	= {arg_,arg_},
+				arg_value 	= {-1,1}, 
+				arg_lim   	= {{-1,0},{0,1}},
+				updatable 	= true, 
+				use_OBB 	= true,
+				use_release_message = {true,false}
+			}
+end
+
+elements = {}
+
+--Spoilers
+elements["PNT_84"] = default_2_position_tumb("Spoiler Arm Switch",devices.SPOILERS, device_commands.spoiler_arm,84)
+
+--ENGINE CONTROL PANEL
+elements["PNT_200"] = default_2_position_tumb("Starter switch",devices.ENGINE, device_commands.push_starter_switch,200)
+elements["PNT_201"] = default_3_position_tumb("Throttle cutoff",devices.ENGINE, device_commands.throttle,201,false)
+
+--ARMAMENT PANEL #35
+elements["PNT_701"] = default_2_position_tumb("Guns switch",devices.WEAPON_SYSTEM, device_commands.arm_gun,701)
+-- elements["PNT_702"]
+elements["PNT_703"] = default_3_position_tumb("Station 1 select",devices.WEAPON_SYSTEM, device_commands.arm_station1,703,false)
+elements["PNT_704"] = default_3_position_tumb("Station 2 select",devices.WEAPON_SYSTEM, device_commands.arm_station2,704,false)
+elements["PNT_705"] = default_3_position_tumb("Station 3 select",devices.WEAPON_SYSTEM, device_commands.arm_station3,705,false)
+elements["PNT_706"] = default_3_position_tumb("Station 4 select",devices.WEAPON_SYSTEM, device_commands.arm_station4,706,false)
+elements["PNT_707"] = default_3_position_tumb("Station 5 select",devices.WEAPON_SYSTEM, device_commands.arm_station5,707,false)
+elements["PNT_708"] = multiposition_switch_limited("Function selector",devices.WEAPON_SYSTEM, device_commands.arm_func_selector,708,6,0.1,true)
+elements["PNT_708"].animated        = {true, true}
+elements["PNT_708"].animation_speed = {0.5, 0.5}
+
+elements["PNT_709"] = default_2_position_tumb("Master armament",devices.WEAPON_SYSTEM, device_commands.arm_master,709)
+
+-- MISC SWITCHES PANEL #36
+elements["PNT_720"] = default_button("Show EXT Fuel", devices.AVIONICS, device_commands.FuelGaugeExtButton, 720)
+
+-- ALTIMETER PANEL #41
+elements["oxygen_valve"] = default_axis("oxygen_valve", devices.AVIONICS, device_commands.AltPressureKnob, 728, 0, 0.05, false, true)
+
+
+for i,o in pairs(elements) do
+	if  o.class[1] == class_type.TUMB or 
+	   (o.class[2]  and o.class[2] == class_type.TUMB) or
+	   (o.class[3]  and o.class[3] == class_type.TUMB)  then
+	   o.updatable = true
+	   o.use_OBB   = true
+	end
+end
