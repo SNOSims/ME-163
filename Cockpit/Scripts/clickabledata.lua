ad6560ca727c08549d6b344fa10eee711dfae126
Alex Wiese <LevelPulse@gmail.com>
2017-11-12 15:57:15 +0000

    Added oxygen valve.

--- Cockpit/Scripts/clickabledata.lua
+++ Cockpit/Scripts/clickabledata.lua
@@ -1,3 +1,18 @@
 dofile(LockOn_Options.script_path.."command_defs.lua")
 dofile(LockOn_Options.script_path.."devices.lua")
 --dofile(LockOn_Options.common_script_path..'localizer.lua')
+
+local gettext = require("i_18n")
+_ = gettext.translate
+
+cursor_mode = 
+{ 
+    CUMODE_CLICKABLE = 0,
+    CUMODE_CLICKABLE_AND_CAMERA  = 1,
+    CUMODE_CAMERA = 2,
+};
+
+clickable_mode_initial_status  = cursor_mode.CUMODE_CLICKABLE
+use_pointer_name			   = true
+
+elements["oxygen_valve"] = default_axis("Oxygen Valve", devices.AVIONICS, device_commands.AltPressureKnob, 728, 0, 0.05, false, true)
