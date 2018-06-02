0b7110dd56626b8a93f395b0ba988b7d5bc40ffe
Alex Wiese <LevelPulse@gmail.com>
2018-06-02 01:42:38 +0100

    Updated scripts

--- Cockpit/Scripts - Copy/config.lua
+++
@@ -1,8 +0,0 @@
-shape 			   = "Cockpit_ME-163B 
-
-EZ42_use_mirror_adjusting = true
-if EZ42_use_mirror_adjusting then
-   EZ42_mirror_adjusting = 0.2 -- degrees K14 LOS rotation
-else
-   EZ42_mirror_adjusting = 0
-end
