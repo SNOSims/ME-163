b506842d4448d6afe2134f8daf619bb92af4abec
Alex Wiese <LevelPulse@gmail.com>
2019-01-23 23:38:38 +0000

    fixed mission

---
+++ Missions/QuickStart/quickstart - test.lua
@@ -0,0 +1,41 @@
+planes = {
+-- Caucasus    
+	{ 
+    name = _('Free Flight'),
+    file = 'Caucasus me_163_free_flight.miz',
+	},
+	{ 
+    name = _('Take Off'),
+    file = 'Caucasus me_163_take_off.miz',
+	},
+	
+--	Nevada
+	{
+    name = _('Cold Start'),
+    file = 'NTTR me_163_cold_start.miz',
+    },
+	{
+    name = _('Take off'),
+    file = 'NTTR me_163_take_off.miz',
+    },
+	{
+    name = _('Free Flight'),
+    file = 'NTTR me_163_free_flight.miz',
+    },
+	
+--	Normandy
+	{
+    name = _('Cold Start'),
+    file = 'Normandy me_163_cold_start.miz',
+    },
+	{
+    name = _('Take off'),
+    file = 'Normandy me_163_take_off.miz',
+    },
+	{
+    name = _('Free Flight'),
+    file = 'Normandy me_163_free_flight.miz',
+    },
+	
+}
+
